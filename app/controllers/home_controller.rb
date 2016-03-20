2ce243315df1723596980433847174b435abbdda
Scott McElveen <scott.mcelveen@hotmail.com>
2016-03-20 19:31:16 +0000

    Restructure ComicVine using a base class for common config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
-require 'ComicVine'
+require 'Publishers'
 
 class HomeController < ApplicationController
     def index
-        @vine = ComicVine.new
-        @publishers = @vine.publishers params["publisher_sort"], params["publisher_offset"]
+        @publishers = Publishers.get params["publisher_sort"], params["publisher_offset"]
     end
 end
